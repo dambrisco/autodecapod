7eeb4032890a9afbff57c412745a7075e835d90e
dambrisco <dambrisco@gmail.com>
2014-12-07 15:38:24 -0700

    Adds --ignore to hg clone

--- decapod-install.sh
+++ decapod-install.sh
@@ -1,4 +1,3 @@
-keys="[hostfingerprints]\ngenpdf.decapod.googlecode.com = fb:5e:a9:2b:52:35:25:c8:05:be:b1:19:41:d2:0f:1a:00:9f:56:a5\nserver.decapod.googlecode.com = fb:5e:a9:2b:52:35:25:c8:05:be:b1:19:41:d2:0f:1a:00:9f:56:a5\nui.decapod.googlecode.com = fb:5e:a9:2b:52:35:25:c8:05:be:b1:19:41:d2:0f:1a:00:9f:56:a5"
 if [ -z $1 ] || [ ! -d $1 ]; then
     echo "Please specify a parent directory"
     exit 1
@@ -12,7 +11,7 @@ else
   fi
 
   while read line; do
-    INSTALLED=$((dpkg -L $line) 2>&1) | grep "is not installed"
+    INSTALLED=$(dpkg -L $line 2>&1) | grep "is not installed"
     if [ $? -eq 0 ]
     then
       echo "Installing: $line"
@@ -20,14 +19,8 @@ else
     fi
   done <dependencies.txt
 
-  if [ ! -f /etc/mercurial/hgrc ] || (
-    ! grep "genpdf\.decapod" /etc/mercurial/hgrc >/dev/null 2>&1 &&
-    ! grep "server\.decapod" /etc/mercurial/hgrc >/dev/null 2>&1 &&
-    ! grep "ui\.decapod" /etc/mercurial/hgrc >/dev/null 2>&1 ); then
-    echo -e "$keys" | sudo tee -a /etc/mercurial/hgrc >> /dev/null
-  fi
   cd $1
-  hg clone https://decapod.googlecode.com/hg/ -r decapod-0.7 decapod-0.7
+  hg clone --insecure https://decapod.googlecode.com/hg/ -r decapod-0.7 decapod-0.7
 
   if [ -d $1/decapod-0.7/install-scripts ]; then
     cd $1/decapod-0.7/install-scripts/

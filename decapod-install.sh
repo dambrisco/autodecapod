8b3910b09857d56aa210fa3662bcb3c5c2573911
Avi Weinstock-Herman <wasbazi@gmail.com>
2014-12-07 14:54:35 -0700

    Update to install ocropus dependencies first

--- decapod-install.sh
+++ decapod-install.sh
@@ -3,9 +3,23 @@ if [ -z $1 ] || [ ! -d $1 ]; then
     echo "Please specify a parent directory"
     exit 1
 else
+  sudo apt-get update
+
   if [ ! $(hash hg 2>/dev/null) ]; then
     sudo apt-get -y install mercurial
+  else
+    sudo apt-get upgrade mercurial
   fi
+
+  while read line; do
+    INSTALLED=$((dpkg -L $line) 2>&1) | grep "is not installed"
+    if [ $? -eq 0 ]
+    then
+      echo "Installing: $line"
+      sudo apt-get -y install $line
+    fi
+  done <dependencies.txt
+
   if [ ! -f /etc/mercurial/hgrc ] || (
     ! grep "genpdf\.decapod" /etc/mercurial/hgrc >/dev/null 2>&1 &&
     ! grep "server\.decapod" /etc/mercurial/hgrc >/dev/null 2>&1 &&
@@ -14,8 +28,10 @@ else
   fi
   cd $1
   hg clone https://decapod.googlecode.com/hg/ -r decapod-0.7 decapod-0.7
+
   if [ -d $1/decapod-0.7/install-scripts ]; then
     cd $1/decapod-0.7/install-scripts/
+
     sudo ./decapod-all.sh
   else
     echo "Unable to clone decapod"
